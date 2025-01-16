01c1bfe377b9defa77afaa6dd598b2ef0f7149a5
Julie Tolve <9919@holbertonstudents.com>
2025-01-16 13:56:00 +0100

    Update uppercase function to convert lowercase letters to uppercase and print the result

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) >= 65 and ord(str) <= 90:
-        print(str, end='\n')
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end='')
+    print()
