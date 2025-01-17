9b095e964d765606a933fce88a3fb8763050d9b7
Julie Tolve <9919@holbertonstudents.com>
2025-01-17 14:20:07 +0100

    Add main guard to infinite_add script for improved module usability

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
 
-sum = 0
-for i in range(1, len(sys.argv)):
-    sum += int(sys.argv[i])
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
 
-print(sum)
+    print(sum)
