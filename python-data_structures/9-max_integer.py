be3370d1b69a4e0acb16bf01c376f49ab73593ba
Julie Tolve <9919@holbertonstudents.com>
2025-01-20 16:33:42 +0100

    Add max_integer function to find the maximum value in a list and main script for testing

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    largest = my_list[0]
+    for val in my_list:
+        if val > largest:
+            largest = val
+    return largest
