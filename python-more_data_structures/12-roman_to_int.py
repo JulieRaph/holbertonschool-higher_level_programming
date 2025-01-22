b0e8c994c7d0909623b3469bf235ec515941d405
Julie Tolve <9919@holbertonstudents.com>
2025-01-22 10:36:02 +0100

    Fix the pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_to_int = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
     result = 0
     prev_value = 0
     if roman_string is None or not str(roman_string):
