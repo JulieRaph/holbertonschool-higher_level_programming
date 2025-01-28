dcd5498f708d8a68865249c0a0b317b9c59086f6
Julie Tolve <9919@holbertonstudents.com>
2025-01-28 09:17:46 +0100

    Add empty Square class with module docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Module that create an empty class square that define a square
+"""
+
+
+class Square:
+    """
+        Function to defines a square
+    """
+    def __init__(self):
+        self
