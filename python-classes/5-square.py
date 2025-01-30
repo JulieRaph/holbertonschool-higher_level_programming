c2fabf7515c3f4feb805d9169431053521737c65
Julie Tolve <9919@holbertonstudents.com>
2025-01-30 09:40:59 +0100

    Refactor Square class: remove size validation and update my_print method for correct output formatting

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -18,10 +18,6 @@ class Square:
         to set it = def size(self, value)
     """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
@@ -53,7 +49,9 @@ class Square:
     """
     def my_print(self):
         if self.__size == 0:
-            print(" ")
+            print()
         else:
             for i in range(self.__size):
-                print("#" * self.__size)
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
