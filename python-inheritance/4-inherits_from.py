bb1cde1674856d987b44c5e320d924a814ee4bc9
Julie Tolve <9919@holbertonstudents.com>
2025-02-03 13:53:45 +0100

    Add docstring to inherits_from function to describe its behavior

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,8 @@ from the specified class ; otherwise False.
 
 
 def inherits_from(obj, a_class):
+    """
+    Print if the object is an instance of a class that
+    inherited from (directly or indirectly)
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
