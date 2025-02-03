b63e98b904652d725a840996256f6335f1081c91
Julie Tolve <9919@holbertonstudents.com>
2025-02-03 13:48:46 +0100

    Add docstring to MyList class to describe its functionality and raise conditions

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,15 @@ This module to write a class MyList that inherits from list
 
 
 class MyList(list):
+    """
+    Print a class from list
+    
+    Raise: 
+        TypeError: element must be an integer
+    
+    Test:
+        test file 1-my_list.txt
+    """
     def my_sum(self):
         if not all(isinstance(e, int) for e in self):
             raise TypeError("element must be an integer")
