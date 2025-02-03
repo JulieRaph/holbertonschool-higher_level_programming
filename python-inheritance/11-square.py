c530c8267dcc1bd0c9b426131f54004efd875455
Julie Tolve <9919@holbertonstudents.com>
2025-02-03 17:20:34 +0100

    Add Square class inheriting from BaseGeometry with area calculation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module contains a class Square that inherits from Rectangle
+(9-rectangle.py)
+(task based on 10-square.py)
+"""
+
+
+class BaseGeometry:
+    """
+    Implementation integer_validator and area method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Square(BaseGeometry):
+    """
+    Class representing a square, inheriting from BaseGeometry
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        return (f"[Square] {self.__size} / {self.__size}")
+
+    @property
+    def size(self):
+        return self.__size
