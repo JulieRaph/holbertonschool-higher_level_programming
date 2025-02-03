a3e19a5b9b520b5c5b7629f088523e3b89bb9e5d
Julie Tolve <9919@holbertonstudents.com>
2025-02-03 12:03:35 +0100

    Add type checks for integer elements in MyList methods my_sum and print_sorted

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,11 @@ This module to write a class MyList that inherits from list
 
 class MyList(list):
     def my_sum(self):
+        if not all(isinstance(e, int) for e in self):
+            raise TypeError("element must be an integer")
         return sum(i * e for i, e in enumerate(self))
 
     def print_sorted(self):
+        if not all(isinstance(e, int) for e in self):
+            raise TypeError("element must be an integer")
         print(sorted(self))
