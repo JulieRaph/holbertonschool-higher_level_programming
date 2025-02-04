5715279f61177daad917aaa59b2f6e8e4f1aad75
Julie Tolve <9919@holbertonstudents.com>
2025-02-04 15:38:20 +0100

    Enhance inherits_from function documentation with argument and return details

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,6 +10,14 @@ def inherits_from(obj, a_class):
     """
     Print if the object is an instance of a class that
     inherited from (directly or indirectly)
+
+    Args:
+        obj: The object to check
+        a_class: The class to compare against
+
+    Return:
+        True if obj is an instance of a class that inherited from a_class;
+        otherwise False.
     """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
