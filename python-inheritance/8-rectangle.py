04cedfa2a7ec3febb6f16e172837e678044a9239
Julie Tolve <9919@holbertonstudents.com>
2025-02-04 16:12:49 +0100

    Refactor Rectangle class docstring for clarity and remove unnecessary comments

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,24 +4,15 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Print a class BaseGeometry with an integer_validator method and
-    a private instance
+    """Print a class BaseGeometry with an integer_validator method and"""
 
-    Args:
+    def __init__(self, width, height):
+        """
+        Args:
         width: a private instance positive int validated by integer_validator
         height: a private instance positive int validated by integer_validator
-    """
-    def __init__(self, width, height):
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
-
-    @property
-    def height(self):
-        return self.__height
