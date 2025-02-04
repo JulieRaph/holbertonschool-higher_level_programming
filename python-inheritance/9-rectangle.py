797851fdc77ce146dbbda663663b93a3e0ccf99c
Julie Tolve <9919@holbertonstudents.com>
2025-02-04 16:19:35 +0100

    Refactor Rectangle class docstring for clarity and remove redundant comments

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,38 +1,21 @@
 #!/usr/bin/python3
-"""
-This module contains a class Rectangle that inherits from BaseGeometry
-(7-base_geometry.py)
-(task based on 8-rectangle.py)
-"""
-
-
-class BaseGeometry:
-    """
-    Implemantation 7-base_geometry.py function
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+"""This module contains a class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Print a rectangle with an area of the rectangle
+    """Print a rectangle with an area of the rectangle"""
 
-    Args:
-        width: private positive integers validated by integer_validator
-        height: private positive integers validated by integer_validator
-
-    Return:
-        area of the rectangle
-        __str__ function to divide width and height
-    """
     def __init__(self, width, height):
+        """
+        Args:
+            width: private positive integers validated by integer_validator
+            height: private positive integers validated by integer_validator
+
+        Return:
+            area of the rectangle
+            __str__ function to divide width and height
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
@@ -43,11 +26,3 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         return (f"[Rectangle] {self.__width}/{self.__height}")
-
-    @property
-    def width(self):
-        return self.__width
-
-    @property
-    def height(self):
-        return self.__height
