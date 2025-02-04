7c21505e9494b3ff6ebfc944920eb3ca234221ce
Julie Tolve <9919@holbertonstudents.com>
2025-02-04 15:43:31 +0100

    Refactor inherits_from module docstring for clarity

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-This module to write a function that returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
-"""
+"""This module to write a function that True if the object is an instance"""
 
 
 def inherits_from(obj, a_class):
