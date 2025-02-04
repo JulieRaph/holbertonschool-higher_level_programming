6f57dd82dc52d351596816ba56a36e4ee833c957
Julie Tolve <9919@holbertonstudents.com>
2025-02-04 15:45:56 +0100

    Refactor MyList class documentation for clarity and consistency

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""
-This module to write a class MyList that inherits from list
-"""
+"""This module to write a class MyList that inherits from list"""
 
 
 class MyList(list):
     """
     Print a class from list
-    
-    Raise: 
+
+    Raise:
         TypeError: element must be an integer
-    
+
     Test:
         test file 1-my_list.txt
     """
     def my_sum(self):
+        """print sorted list"""
         if not all(isinstance(e, int) for e in self):
             raise TypeError("element must be an integer")
         return sum(i * e for i, e in enumerate(self))
 
     def print_sorted(self):
+        """print sorted list"""
         if not all(isinstance(e, int) for e in self):
             raise TypeError("element must be an integer")
         print(sorted(self))
