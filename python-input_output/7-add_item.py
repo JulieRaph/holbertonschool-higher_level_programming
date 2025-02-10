7f1b909b3f69ba6fb022fb96f6bdf336bf0ae249
Julie Tolve <9919@holbertonstudents.com>
2025-02-10 15:07:20 +0100

    Function to load, add and save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module to load, add, save
+"""
+
+
+import json
+import sys
+from os.path import exists
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+"""
+Function to write a script that adds all arguments to a 
+Python list, and then save them to a file
+"""
+
+
+filename = "add_item.json"
+
+my_list = []
+
+if exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+save_to_json_file(my_list, filename)
