ad7d540a720c223033ceabf5126968dd5a7a1509
Julie Tolve <9919@holbertonstudents.com>
2025-02-11 17:03:39 +0100

    Function to serializing and deserializing with XML

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Serializing and Deserializing with XML"""
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """serialize a Python dictionnary to XML and save it to a file"""
+
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+def deserialize_from_xml(filename):
+    """deserialize XMl to a Python dictionary"""
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        data = {}
+
+        for child in root:
+            key = child.tag
+            value = child.text
+
+            try:
+                value = int(value)
+            except ValueError:
+                try:
+                    value = float(value)
+
+                except ValueError:
+                    if value.lower() == "true":
+                        value = True
+                    elif value.lower() == "false":
+                        value = False
+
+            data[key] = value
+        return data
+
+    except(FileNotFoundError, ET.ParseError, Exception) as e:
+        return None
