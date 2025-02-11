472db1c1a40d0662f09aafa4d9f64808f02b66a9
Julie Tolve <9919@holbertonstudents.com>
2025-02-11 16:07:48 +0100

    Fix pycodestyle and define data

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,6 +8,7 @@ import json
 
 def convert_csv_to_json(csv_file):
 
+    data = []
     try:
         with open(csv_file, 'r', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -16,7 +17,7 @@ def convert_csv_to_json(csv_file):
 
         with open('data.json', 'w', encoding="utf-8") as jsonfile:
             json.dump(data, jsonfile, indent=4)
-  
+
         return True
 
     except (FileNotFoundError, Exception) as e:
