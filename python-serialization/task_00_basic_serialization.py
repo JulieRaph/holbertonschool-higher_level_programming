1eac6584c142062a5fda969362d732beb66aead0
Julie Tolve <9919@holbertonstudents.com>
2025-02-12 10:33:59 +0100

    Fix code with json

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,17 +6,18 @@ serialize a Python dictionnary to a JSON file and deserialize the JSON
 
 
 import json
-import pickle
 
 
 def serialize_and_save_to_file(data, filename):
     """serialize a Python dictionnary to a JSON file"""
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w+", encoding="utf-8") as file:
+        file.write(json.dumps(data))
+    pass
 
 
 def load_and_deserialize(filename):
     """deserialize the JSON file to recreate the Python dictionnary"""
-    with open(filename, "rb") as file:
-        loaded_data = pickle.load(file)
+    with open(filename, "r", encoding="utf-8") as file:
+        loaded_data = json.loads(file.read())
     return loaded_data
+    pass
