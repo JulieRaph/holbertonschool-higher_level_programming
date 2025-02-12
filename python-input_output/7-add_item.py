fe29eea3a888eb71e780c9511fa7a5af7066ea47
Julie Tolve <9919@holbertonstudents.com>
2025-02-12 09:18:55 +0100

    Fix code and main

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -17,16 +17,16 @@ Function to write a script that adds all arguments to a
 Python list, and then save them to a file
 """
 
+if __name__ == "__main__":
+    filename = "add_item.json"
 
-filename = "add_item.json"
-
-my_list = []
-
-if exists(filename):
-    my_list = load_from_json_file(filename)
-else:
     my_list = []
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-save_to_json_file(my_list, filename)
+    if exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+
+    for arg in sys.argv[1:]:
+        my_list.append(arg)
+    save_to_json_file(my_list, filename)
