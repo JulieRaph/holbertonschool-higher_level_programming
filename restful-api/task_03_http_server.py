7940fd4fc51955669434f349abb8590c456fab62
Julie Tolve <9919@holbertonstudents.com>
2025-02-18 11:48:53 +0100

    Add task_03_http_server.py to implement a simple API using http.server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Develop a simple API using Python with the `http.server` module"""
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+class MyRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json_data.encode("utf-8"))
+        elif self.path == '/status':
+            status = ("OK")
+            json_status = json.dumps(status)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(json_status.encode("utf-8"))
+        elif self.path == '/info':
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            json_info = json.dumps(info)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(json.info.encode("utf-8"))
+        elif self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+host = "localhost"
+port = 8000
+httpd = HTTPServer((host, port), MyRequestHandler)
+print("Starting server on http://localhost:8000")
+httpd.serve_forever()
