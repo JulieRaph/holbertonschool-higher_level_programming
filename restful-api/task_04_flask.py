9b8112bc983ed70c066c4d5ca89347fdd223b779
Julie Tolve <julie.tolve@gmail.com>
2025-02-19 13:34:29 +0100

    Implement a simple Flask API with user management endpoints

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Develop a Simple API using Python with Flask"""
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+users = {
+        "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+        "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+    }
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route("/data")
+def users():
+    return jsonify(list(users.keys()))
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>")
+def get_user(username):
+    if username in users:
+        return jsonify(users.username)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route("/add_user")
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+    if username in users:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
+
+if __name__ == "__main__":
+    app.run()
