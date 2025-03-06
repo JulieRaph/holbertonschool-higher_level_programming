0e8acaea40d17148ea7f771baa075516acdd0bef
JulieRaph <julie.tolve@gmail.com>
2025-03-06 16:03:57 +0100

    Add docstring to State class for clarity on table structure

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,6 +11,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Create name table and colonnes"""
     __tablename__ = 'states'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
