082114cefb8ca7d086c6cfb64d7aee6c3175278f
JulieRaph <julie.tolve@gmail.com>
2025-04-02 19:58:59 +0200

    Update greeting message display method to use innerHTML instead of append

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const response = await fetch('https://hellosalut.stefanbohacek.dev/?lang=fr');
         const data = await response.json();
         const helloElement = document.querySelector('#hello');
-        helloElement.append(data.hello);
+        helloElement.innerHTML = data.hello;
     } catch (error) {
         console.error('Error fetching data:', error);
     }});
-
